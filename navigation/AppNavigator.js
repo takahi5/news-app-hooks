2a5e1bfebc363f8d2e8b6d601b6a4147b5686732
Takahiko Wada <wada.takahiko@gmail.com>
2019-11-11 21:51:59 +0900

    Added Tab Bar Icon

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Icon from '@expo/vector-icons/FontAwesome';
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
 import ClipScreen from '../screens/ClipScreen';
@@ -31,8 +32,22 @@ const ClipStack = createStackNavigator({
 });
 
 const TabNavigator = createBottomTabNavigator({
-  Home: HomeStack,
-  Clip: ClipStack,
+  Home: {
+    screen: HomeStack,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="home" color={tintColor} size={24} />
+      ),
+    },
+  },
+  Clip: {
+    screen: ClipStack,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="bookmark" color={tintColor} size={24} />
+      ),
+    },
+  },
 });
 
 export default createAppContainer(TabNavigator);
