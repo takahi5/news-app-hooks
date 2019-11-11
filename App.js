41da80b798301b0855928c72ebfa8df06c3ac229
Takahiko Wada <wada.takahiko@gmail.com>
2019-11-11 12:30:26 +0900

    Added key

--- App.js
+++ App.js
@@ -14,12 +14,13 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
-  const items = articles.map(article => {
+  const items = articles.map((article, index) => {
     return (
       <ListItem
         imageUrl={article.urlToImage}
         title={article.title}
         author={article.author}
+        key={index}
       />
     );
   });
